74563b35cde74433cf95d996dfb6f9ba2a4233c7
nookismile <stina.novickova@yandex.ru>
2024-03-01 22:41:24 +0300

    add JSDoc

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param a
+ * @param b
+ * @returns {*}
+ */
+
+function sum(a, b) {
+    return a + b;
+}
+
+/**
+ *
+ * @param param
+ * @returns {number}
+ */
+function count(param){
+    let result = 1;
+    for (let i = 1; i <= a; i++) {
+        result = result * i;
+    }
+    return result;
+}
