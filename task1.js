cd8c3068e26c7106692e6f058458ed7e8d1a1bef
nookismile <52919340+nookismile@users.noreply.github.com>
2024-04-19 10:51:45 +0300

    add throw new Error

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 /**
  * Function for sort string
- * @param arr
- * @param param
+ * @param {string[]} arr
+ * @param {'asc' | 'desc'} param
  * @returns {*[]}
  */
 
@@ -12,6 +12,8 @@ function sortStrings(arr, param = "asc") {
             return a.localeCompare(b, ["ru", "en"], {caseFirst: 'upper'});
         } else if (param === 'desc') {
             return b.localeCompare(a, ["ru", "en"], {caseFirst: 'upper'});
+        } else if (param !== 'asc' || param !== 'desc'){
+          throw new Error('Invalid param!');
         }
     })
 }
