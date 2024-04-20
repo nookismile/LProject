2c3d4c5269848495391fdaba1ff2835a31a34896
nookismile <52919340+nookismile@users.noreply.github.com>
2024-04-20 12:23:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-/**
- *
- * @param a
- * @param b
- * @returns {*}
- */
+export default class NotificationMessage {
+    static notification;
+    timer;
 
-function sum(a, b) {
-    return a + b;
-}
-
-/**
- *
- * @param param
- * @returns {number}
- */
-function count(param){
-    let result = 1;
-    for (let i = 1; i <= a; i++) {
-        result = result * i;
+    constructor(info = '', {type = 'success', duration = '1000'} = {}) {
+        this.info = info;
+        this.duration = duration;
+        this.type = type;
+        this.create();
     }
-    return result;
+
+    create() {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = `
+      <div class="notification">
+        <div class="timer"></div>
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+      </div>`;
+        wrapper.firstElementChild.classList.add(this.type);
+        this.element = wrapper.firstElementChild;
+    }
+
+    show(element) {
+        if (NotificationMessage.notification) {
+            NotificationMessage.notification.remove();
+        }
+        if (element) {
+            this.element = element;
+        }
+        document.body.prepend(this.element);
+        this.timer = setTimeout(() => this.remove(), this.duration);
+        NotificationMessage.notification = this;
+    }
+
+    remove() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+        this.element.remove();
+    }
 }
