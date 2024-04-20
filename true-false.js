a52961b7660cb7afd15f955eff72a4a91ac64230
nookismile <52919340+nookismile@users.noreply.github.com>
2024-04-20 12:31:48 +0300

    Update true-false.js

--- true-false.js
+++ true-false.js
@@ -4,12 +4,9 @@
  * @param {number} k
  * @returns {boolean}
  */
-
-let arr = [10, 15, 3, 7];
-let num = 17;
-
-function sum(arr, k) {
-    for(let i = 0; i < nums.length - 1; i++){
+  
+module.exports = function(nums, k) {  
+     for(let i = 0; i < nums.length - 1; i++){
         for(let j = i + 1; j < nums.length; j++){
             if(nums[i]+nums[j]===k) {
                 return true;
@@ -18,5 +15,3 @@ function sum(arr, k) {
     }
     return false;
 }
-
-sum(arr, num);
